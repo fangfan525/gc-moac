a15b19155a13e5b16147b072abccef0e11183b5b
fangfan <fangfan@bihao.pro>
2018-12-08 17:49:48 +0800

    aaa

--- api/models/Usertibi.js
+++ api/models/Usertibi.js
@@ -8,7 +8,7 @@
 module.exports = {
 
   attributes: {
-    id:           { type: 'number', utoIncrement: true },
+    id:           { type: 'number', autoIncrement: true },
     user_id:      { type: 'number'},
     create_time:  { type: 'string'},
     address:      { type: 'string'},
